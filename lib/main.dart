f2b62f55eaa12bbfbb2d1c2e7e24b86ec8c657bf
smsmsy <smsmsy.dev@gmail.com>
2025-11-04 22:52:46 +0900

    ProviderScopeを追加し、テーマ管理機能をConsumerWidgetに統合

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:twitter_clone/src/theme/theme.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     const color = Color.fromARGB(255, 0, 172, 238);
     return MaterialApp(
       title: 'Flutter Demo',
@@ -23,17 +29,36 @@ class MyApp extends StatelessWidget {
           brightness: Brightness.dark,
         ),
       ),
-      themeMode: ThemeMode.dark,
+      themeMode: ref.watch(customThemeModeProvider),
       home: const Home(),
     );
   }
 }
 
-class Home extends StatelessWidget {
+class Home extends ConsumerWidget {
   const Home({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final themeMode = ref.watch(customThemeModeProvider);
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {
+              ref.read(customThemeModeProvider.notifier).toggle();
+            },
+            icon: Icon(getIconData(themeMode)),
+          ),
+        ],
+      ),
+      body: const Center(child: Text('Hello world!')),
+    );
+  }
+
+  IconData getIconData(ThemeMode themeMode) {
+    return themeMode == ThemeMode.dark
+        ? Icons.brightness_3
+        : Icons.brightness_5;
   }
 }
